347e30e73029c6c18e40f5c7b24ba41ed5de7611
Fabio Hernandez <fuem.dev@gmail.com>
2025-07-16 17:06:51 -0500

    Initial commit – CI/CD setup base React + Vite + TS

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>CI/CD Code</h1>
+      <p>This is a demo app for Continuous Integration and Deployment</p>
+    </div>
+  );
+}
+
+export default App;
